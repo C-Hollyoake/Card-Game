96461869c7ff45be66b140758cbffb4e92176fd5
C-Hollyoake <charlie.h12@hotmail.co.uk>
2022-09-01 19:32:38 +0100

    defined the Hand class

--- OOPCards.py
+++ OOPCards.py
@@ -38,3 +38,34 @@ class Deck:
     
     def shuffle(self):
         random.shuffle(self.deck)
+
+deck = Deck()
+
+class Hand(Deck):
+
+    def __init__(self, label):
+        self.deck = []
+        self.label = label
+        self.win_count = 0
+    
+    def __str__(self):
+        return self.label + ': ' + ' '.join([str(card) for card in self.deck])
+    
+    def get_label(self):
+        return self.label
+    
+    def get_win_count(self):
+        return self.win_count
+    
+    def round_winner(self):
+        self.win_count = self.win_count + 1
+
+hand = Hand("Player")
+
+hand.add_card(deck.pop_card())
+hand.add_card(deck.pop_card())
+hand.add_card(deck.pop_card())
+hand.add_card(deck.pop_card())
+hand.add_card(deck.pop_card())
+
+print(hand)
